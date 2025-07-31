d454e33b2b40172e1d1186e1bb3685989d56c4d3
phulrich4 <pipoulrich4@gmail.com>
2025-07-31 17:33:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("LexMind - KI-Assistent für Juristen🎈")
+st.title("LexMind - KI-Assistent für Juristen")
 st.write(
     "Let's start building! Hier entsteht grossartiges."
 )
