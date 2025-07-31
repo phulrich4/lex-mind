c2b5ef26587e7e3cb26b51bb26dfb5fe2610f927
phulrich4 <pipoulrich4@gmail.com>
2025-07-31 17:32:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("LexMind - Dein KI-Assistent im Alltag🎈")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! Hier entsteht grossartiges."
 )
