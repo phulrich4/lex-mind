d42c369fe6bbef48c0cd1573acfaa86ad9396154
phulrich4 <pipoulrich4@gmail.com>
2025-08-01 19:27:39 +0200

    Update search_tab.py

--- tabs/search_tab.py
+++ tabs/search_tab.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def render():
     query = st.text_input("Was suchen Sie?", placeholder= "z. B. Kapitalerhöhung, Abtretung, etc.")
-    st.markdown("#Ergebnisse")
+    st.markdown("")
     for i in range(3):
         st.write(f"📄 Treffer {i+1} – Beispielinhalt ...")
         st.button("📥 Vorlage herunterladen", key=f"download_{i}")
