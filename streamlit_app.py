2386196a11068b31cf68e3c28c3e09b960bd0989
phulrich4 <pipoulrich4@gmail.com>
2025-08-01 15:32:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ if "page" not in st.session_state:
 
 # Sidebar mit fixen Buttons für Navigation
 with st.sidebar:
-    st.markdown("## LexMind")
+    st.markdown("## ⚖️LexMind")
     if st.button("🔍 Suche"):
         st.session_state.page = "Suche"
     if st.button("📚 Vorlagenübersicht"):
