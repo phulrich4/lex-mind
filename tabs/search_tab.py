a7b710bb49e27388baf1c96ed996051e97ec7a35
phulrich4 <pipoulrich4@gmail.com>
2025-08-01 19:27:54 +0200

    Update search_tab.py

--- tabs/search_tab.py
+++ tabs/search_tab.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def render():
-    query = st.text_input("Was suchen Sie?", placeholder= "z. B. Kapitalerhöhung, Abtretung, etc.")
+    query = st.text_input("", placeholder= "z. B. Kapitalerhöhung, Abtretung, etc.")
     st.markdown("")
     for i in range(3):
         st.write(f"📄 Treffer {i+1} – Beispielinhalt ...")
