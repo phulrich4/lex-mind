b41d6a9e87b5869d9058aa13a1f70d80ee32f00c
phulrich4 <pipoulrich4@gmail.com>
2025-08-01 19:11:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title="LexMind", layout="wide")
-
-st.title("LexMind – KI-Assistent für Juristen")
-st.write("Bitte wähle eine Funktion aus.")
-
-if "my_input" not in st.session_state:
-    st.session_state["my_input"] = ""
-
-my_input = st.text_input("Input a text here", st.session_state["my_input"])
-submit = st.button("Submit")
-if submit:
-    st.session_state["my_input"] = my_input
-    st.write("You have entered: ", my_input)
-
+st.title("LexMind - KI-Assistent für Juristen🎈")
+st.title("LexMind - KI-Assistent für Juristen")
+st.write(
+    "Let's start building! Hier entsteht grossartiges."
+)
